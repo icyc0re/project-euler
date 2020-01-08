35e1d5f0c9c4aef3bc44ada94bb172984dbe0bc6
Tiziano Signo <tizianosigno@peterssoftware.de>
2020-01-08 18:49:24 +0100

    add python solution to problem 11

---
+++ src/problem_011.py
@@ -0,0 +1,52 @@
+import os
+import operator
+from functools import reduce
+
+def load_input(filename):
+    with open(filename) as fr:
+        content = [list(map(int, line.strip().split(' '))) for line in fr.readlines()]
+    return content
+
+def compute_highest(values, k):
+    highest = -1
+    for line in values:
+        for i in range(len(line) - k):
+            product = reduce(operator.mul, line[i:i+k])
+            if product > highest:
+                highest = product
+    return highest
+
+# expects NxN matrix
+def euler_011(values):
+    k = 4
+    size = len(values)
+
+    # compute highest horizontal
+    h1 = compute_highest(values, k)
+    
+    # compute highest vertical
+    values2 = []
+    for row, _ in enumerate(values):
+        values2.append([val[row] for val in values])
+    h2 = compute_highest(values2, k)
+
+    # compute highest diagonal (top left - bottom-right)
+    values3 = [[1] * size for _ in range(size*2)]
+    for r, row in enumerate(values):
+        for c, n in enumerate(row):
+            values3[size-r+c][r] = n
+    h3 = compute_highest(values3, k)
+
+    # compute highest diagonal (top right - bottom left)
+    values4 = [[1] * size for _ in range(size*2)]
+    for r, row in enumerate(values):
+        for c, n in enumerate(row):
+            values4[r+c][r] = n
+    h4 = compute_highest(values4, k)
+
+    return max([h1, h2, h3, h4])
+
+if __name__ == '__main__':
+    input_filename = os.path.join(os.path.dirname(__file__), 'input/problem_011.txt')
+    input_value = load_input(input_filename)
+    print(euler_011(input_value))
