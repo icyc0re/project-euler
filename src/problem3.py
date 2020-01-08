71e3dc0cebab3c2a4d77f18cdc744c9619eef3e6
Tiziano Signo <tizianosigno@peterssoftware.de>
2020-01-08 10:16:51 +0100

    Simplify and improve solution performance

--- src/problem3.py
+++ src/problem3.py
@@ -1,16 +1,13 @@
-import math
-
+# No need to check for primes, non-primes are already multiples of smaller values
 def euler_003(value):
-    # generate primes until 1000000
-    primes = [False, False, True] + [True] * 999998
-    for n, is_prime in enumerate(primes):
-        if is_prime:
-            if value % n == 0:
-                value //= n
-                if value == 1:
-                    return n
-            for k in range(n * 2, len(primes), n):
-                primes[k] = False
+    n = 2
+    while value > 1:
+        if value % n == 0:
+            value //= n
+        else:
+            n += 1
+    return n
 
 if __name__ == '__main__':
-    print('Highest factor: {}'.format(euler_003(600851475143)))
+    result = euler_003(600851475143)
+    print('Highest factor: {}'.format(result))
