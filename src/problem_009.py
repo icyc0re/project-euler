23df1886cd12119da1ccee3050343354030d572b
Tiziano Signo <tizianosigno@peterssoftware.de>
2020-01-09 10:09:56 +0100

    minor refactor of problem 9

--- src/problem_009.py
+++ src/problem_009.py
@@ -2,10 +2,10 @@ def is_pythagorean_triplet(a, b, c):
     return a ** 2 + b ** 2 == c ** 2
 
 def euler_009():
-    val = 1000
-    for a in range(1, val // 3):
-        for b in range(a + 1, (val - a) // 2):
-            c = 1000 - a - b
+    n_sum = 1000
+    for a in range(1, n_sum // 3):
+        for b in range(a + 1, (n_sum - a) // 2):
+            c = n_sum - a - b
             if is_pythagorean_triplet(a, b, c):
                 print(a, b, c, a * b * c)
 
